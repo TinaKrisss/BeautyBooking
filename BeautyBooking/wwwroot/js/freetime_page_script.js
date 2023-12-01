e371e3ba2864c17b8afcaf85a33b3fb9d34561eb
TinaKrisss <pan4enko.kris@gmail.com>
2023-12-01 23:35:01 +0200

    free time selection script added, styles edited

---
+++ BeautyBooking/wwwroot/js/freetime_page_script.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $(".free-date").click(function () {
+
+        $(".free-date").removeClass("date-selected");
+        $(".free-time").removeClass("time-selected");
+
+        $(this).addClass("date-selected");
+
+        var selectedDate = $(this).attr("id");
+
+        $(".free-time").addClass("hidden");
+
+        var matchingBlocks = $("." + selectedDate);
+
+        matchingBlocks.removeClass("hidden");
+    });
+
+    $(".free-time").click(function () {
+        $(".free-time").removeClass("time-selected");
+
+        $(this).addClass("time-selected");
+    });
+});
