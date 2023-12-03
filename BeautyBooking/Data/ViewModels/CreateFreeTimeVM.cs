1b5d43d418fe913a4f08c2b565d765fdff63f4f3
TinaKrisss <pan4enko.kris@gmail.com>
2023-12-03 16:58:03 +0200

    CreateFreeTime-view added.

---
+++ BeautyBooking/Data/ViewModels/CreateFreeTimeVM.cs
@@ -0,0 +1,11 @@
+﻿namespace BeautyBooking.Data.ViewModels
+{
+	public class CreateFreeTimeVM
+	{
+		public int Id { get; set; }
+		public string? ProfilePhotoURL { get; set; }
+		public string? Surname { get; set; }
+		public string? Name { get; set; }
+		public DateTime DateAndTime { get; set; }
+	}
+}
