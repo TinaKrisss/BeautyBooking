f833bb914bd8cb55a37415878a312d8f283d11f9
TinaKrisss <pan4enko.kris@gmail.com>
2023-12-05 21:28:57 +0200

    FreeTime selection edited

--- BeautyBooking/wwwroot/js/services_page_script.js
+++ BeautyBooking/wwwroot/js/services_page_script.js
@@ -1,26 +1,4 @@
 ﻿        $('.service-item [type="checkbox"]').change(function() {
             let $this = $(this);
             $this.closest('label').find('.select-type').toggleClass('active');
-
-            //calculateTotals();
         });
-
-        //function calculateTotals() {
-        //    const $wrapper = $('#services_form');
-        //    let $checkedServices = $('.select-type.active', $wrapper);
-        //    let totalTime = 0;
-        //    let totalSum = 0;
-
-        //    $checkedServices.each(function(i, item) {
-        //        totalTime += +$('[name=duration]', item).val();
-        //        totalSum += +$('.price:not(.hide)', item).text();
-        //    });
-
-        //    totalSum = isNaN(totalSum) ? 0 : totalSum;
-
-        //    let formattedTime = (new Date(totalTime * 1000)).toISOString().substr(11, 5);
-
-        //    $('#total-time').text(formattedTime);
-        //    $('[name="total_time"]').val(totalTime);
-        //    $('#total-sum').text(totalSum);
-        //}
