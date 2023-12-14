7adeb322143e1c48b1ed8859417c26b315ee7d89
Vondert <25042018avic@gmail.com>
2023-12-14 19:42:14 +0200

    clients controller added

---
+++ BeautyBooking/Controllers/ClientsController.cs
@@ -0,0 +1,40 @@
+﻿using BeautyBooking.Data.Interfaces;
+using BeautyBooking.Data.Static;
+using BeautyBooking.Data.ViewModels;
+using BeautyBooking.Data.Enums;
+using BeautyBooking.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BeautyBooking.Controllers
+{
+    public class ClientsController : Controller
+    {
+        private readonly IClientsService _service;
+
+        public ClientsController(IClientsService service)
+        {
+            _service = service;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var clients = await _service.GetAllAsync();
+            if (clients == null) return View("NotFound");
+            return View(clients);
+        }
+        public async Task<IActionResult> Details(int id)
+        {
+            var clientDetails = await _service.GetByIdAsync(id);
+
+            if (clientDetails == null) return View("NotFound");
+            return View(clientDetails);
+        }
+        public async Task<IActionResult> Delete(int id)
+        {
+            var client = await _service.GetByIdAsync(id);
+            if (client == null) return View("NotFound");
+
+            await _service.DeleteAsync(id);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
